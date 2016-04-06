8e0bb0adb03221aa8a7c12f047e47a352926eff3
GuillaumeFallet <chibre001@gmail.com>
2016-04-06 12:37:55 +0200

    Test - structure

--- src/map.js
+++ src/map.js
@@ -4,20 +4,29 @@
 * Created by Pydd on 05.04.2016.
 */
 
-var map;
-function initMap() {
+    var map;
+
+    // method to initialize the map
+    function initMap()
+    {
 
 
-    map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 46.293, lng: 7.533},
-    mapTypeId: google.maps.MapTypeId.SATELLITE,
-    zoom: 14,
-    disableDefaultUI: true,
-    rotateControl: true
+        map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 46.32, lng: 7.53},
+        mapTypeId: google.maps.MapTypeId.SATELLITE,
+        zoom: 13,
+        disableDefaultUI: true,
+        rotateControl: true,
+        minZoom: 12,
+        maxZoom: 15
 
-});
-    
+        });
+    }
 
-}
+    // method to center the map on the network of Tseuzier
+    function centerTseuzier(){
+
+        map.setCenter({lat: 46.32, lng: 7.53}) ;
+    }
 
 
