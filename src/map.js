71b4ba524d90214c347dec257584582238484319
GuillaumeFallet <chibre001@gmail.com>
2016-04-08 11:37:45 +0200

    Ajout des markers pour captages 1 2 3

--- src/map.js
+++ src/map.js
@@ -4,8 +4,6 @@
 * Created by Pydd on 05.04.2016.
 */
 
-    var map;
-
 
     // method to initialize the map
     function initMap()
@@ -22,23 +20,15 @@
         maxZoom: 15
 
         });
-
-        var kmzLayer  = new google.maps.KmlLayer({ url : 'tseuzier.kmz'});
-
-        kmzLayer.setMap(map) ;
-
-
-        var coord_lac =  {lat: 46.350560, lng: 7.430909} ;
-        var img_lac = {
+            coord_lac =  {lat: 46.350560, lng: 7.430909} ;
+            img_lac = {
             url: "images/lake_full.png",
-
             origin: new google.maps.Point(0,0),
             anchor: new google.maps.Point(20,25),
             scaledSize: new google.maps.Size(40, 40)
         } ;
 
-
-        var marker_lake_tseuzier = new google.maps.Marker({
+            marker_lake_tseuzier = new google.maps.Marker({
             position: coord_lac,
             map: map,
             icon: img_lac,
@@ -55,22 +45,39 @@
             info_lake_tseuzier.open(map,marker_lake_tseuzier)
         }) ;
 
-        var img_captage = {
-            url: "images/captage.png",
 
+        coord_capt1 =  {lat: 46.346562, lng: 7.474394} ;
+        coord_capt2 =  {lat: 46.342899, lng: 7.506874} ;
+        coord_capt3 =  {lat: 46.355108, lng: 7.533259} ;
+
+        img_captage = {
+            url: "images/captage.png",
             origin: new google.maps.Point(0,0),
             anchor: new google.maps.Point(20,25),
             scaledSize: new google.maps.Size(40, 40)
         } ;
 
-        var marker_captage_1 = new google.maps.Marker({
-            position: new google.maps.LatLng(46.203421, 7.302434),
+
+        marker_capt1_tseuzier = new google.maps.Marker({
+            position: coord_capt1,
             map: map,
             icon: img_captage,
-            title: 'captage1'
+            title: 'Captage 1'
         });
 
-        //
+        marker_capt2_tseuzier = new google.maps.Marker({
+            position: coord_capt2,
+            map: map,
+            icon: img_captage,
+            title: 'Captage 2'
+        });
+
+        marker_capt3_tseuzier = new google.maps.Marker({
+            position: coord_capt3,
+            map: map,
+            icon: img_captage,
+            title: 'Captage 3'
+        });
 
 
     }
@@ -80,6 +87,68 @@
 
         map.setCenter({lat: 46.32, lng: 7.53}) ;
         map.setZoom(13) ;
+
+    }
+
+    function nextMonth()
+    {
+        month ++ ;
+        if (month==13)
+            month = 1 ;
+
+        switch (month)
+        {
+            case 1 :
+                img_lac.url = "images/lake_empty.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 2 :
+                img_lac.url = "images/lake_nearlyempty.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 3 :
+                img_lac.url = "images/lake_nearlyfull.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 4 :
+                img_lac.url = "images/lake_full.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 5 :
+                img_lac.url = "images/lake_nearlyfull.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 6 :
+                img_lac.url = "images/lake_nearlyempty.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 7 :
+                img_lac.url = "images/lake_empty.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 8 :
+                img_lac.url = "images/lake_nearlyempty.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 9 :
+                img_lac.url = "images/lake_nearlyfull.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 10 :
+                img_lac.url = "images/lake_full.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 11 :
+                img_lac.url = "images/lake_nearlyfull.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 12 :
+                img_lac.url = "images/lake_nearlyempty.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+        }
+
+
 
     }
 
