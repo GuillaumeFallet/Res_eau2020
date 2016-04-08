2b4ae319a5e5f5b290d32467c260dad8b8e5ea2c
GuillaumeFallet <chibre001@gmail.com>
2016-04-08 09:49:31 +0200

    Test - changement de mois

--- src/map.js
+++ src/map.js
@@ -3,14 +3,12 @@
 /**
 * Created by Pydd on 05.04.2016.
 */
-
+    var test = 4 ;
     var map;
-
+    var month = 0 ;
     // method to initialize the map
     function initMap()
     {
-
-
         map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 46.32, lng: 7.53},
         mapTypeId: google.maps.MapTypeId.SATELLITE,
@@ -26,23 +24,24 @@
 
         kmzLayer.setMap(map) ;
 
-
+        test = 8 ;
         var coord_lac =  {lat: 46.350560, lng: 7.430909} ;
-        var img_lac = {
+        img_lac = {
             url: "images/lake_full.png",
-
             origin: new google.maps.Point(0,0),
             anchor: new google.maps.Point(20,25),
             scaledSize: new google.maps.Size(40, 40)
         } ;
 
 
-        var marker_lake_tseuzier = new google.maps.Marker({
+         marker_lake_tseuzier = new google.maps.Marker({
             position: coord_lac,
             map: map,
             icon: img_lac,
             title: 'Niveau du lac de Tseuzier'
         });
+
+        
 
         var lake_tseuzier_level = "Niveau du lac de Tseuzier (PLACEHOLDER)"
         var info_lake_tseuzier = new google.maps.InfoWindow({
@@ -60,6 +59,68 @@
 
         map.setCenter({lat: 46.32, lng: 7.53}) ;
         map.setZoom(13) ;
+
+    }
+
+    function nextMonth()
+    {
+        month ++ ;
+        if (month==13)
+            month = 1 ;
+
+        switch (month)
+        {
+            case 1 :
+                img_lac.url = "images/lake_empty.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 2 :
+                img_lac.url = "images/lake_nearlyempty.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 3 :
+                img_lac.url = "images/lake_nearlyfull.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 4 :
+                img_lac.url = "images/lake_full.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 5 :
+                img_lac.url = "images/lake_nearlyfull.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 6 :
+                img_lac.url = "images/lake_nearlyempty.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 7 :
+                img_lac.url = "images/lake_empty.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 8 :
+                img_lac.url = "images/lake_nearlyempty.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 9 :
+                img_lac.url = "images/lake_nearlyfull.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 10 :
+                img_lac.url = "images/lake_full.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 11 :
+                img_lac.url = "images/lake_nearlyfull.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+            case 12 :
+                img_lac.url = "images/lake_nearlyempty.png" ;
+                marker_lake_tseuzier.icon = img_lac ;
+                break ;
+        }
+
+
 
     }
 
