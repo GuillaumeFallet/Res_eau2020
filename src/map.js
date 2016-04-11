6b93ce25de180da4cfd800a6c8f9c79c396f7a06
GuillaumeFallet <chibre001@gmail.com>
2016-04-11 10:04:46 +0200

    Ajout des markers pour captages 1 2 3

--- src/map.js
+++ src/map.js
@@ -4,6 +4,8 @@
 * Created by Pydd on 05.04.2016.
 */
 
+    month = 0 ;
+
 
     // method to initialize the map
     function initMap()
@@ -20,9 +22,27 @@
         maxZoom: 15
 
         });
-            coord_lac =  {lat: 46.350560, lng: 7.430909} ;
-            img_lac = {
+        coord_lac =  {lat: 46.350560, lng: 7.430909} ;
+        img_lake_full = {
             url: "images/lake_full.png",
+            origin: new google.maps.Point(0,0),
+            anchor: new google.maps.Point(20,25),
+            scaledSize: new google.maps.Size(40, 40)
+        } ;
+        img_lake_nearly_full = {
+            url: "images/lake_nearlyfull.png",
+            origin: new google.maps.Point(0,0),
+            anchor: new google.maps.Point(20,25),
+            scaledSize: new google.maps.Size(40, 40)
+        } ;
+        img_lake_nearly_empty = {
+            url: "images/lake_nearlyempty.png",
+            origin: new google.maps.Point(0,0),
+            anchor: new google.maps.Point(20,25),
+            scaledSize: new google.maps.Size(40, 40)
+        } ;
+        img_lake_empty = {
+            url: "images/lake_empty.png",
             origin: new google.maps.Point(0,0),
             anchor: new google.maps.Point(20,25),
             scaledSize: new google.maps.Size(40, 40)
@@ -31,7 +51,7 @@
             marker_lake_tseuzier = new google.maps.Marker({
             position: coord_lac,
             map: map,
-            icon: img_lac,
+            icon: img_lake_full,
             title: 'Niveau du lac de Tseuzier'
         });
 
@@ -92,64 +112,36 @@
 
     function nextMonth()
     {
-        month ++ ;
-        if (month==13)
-            month = 1 ;
+         month ++ ;
+        if (month==4)
+            month = 0 ;
 
-        switch (month)
+        changeLakeLevel(month) ;
+    }
+
+    function changeLakeLevel(lvl)
         {
-            case 1 :
-                img_lac.url = "images/lake_empty.png" ;
-                marker_lake_tseuzier.icon = img_lac ;
-                break ;
-            case 2 :
-                img_lac.url = "images/lake_nearlyempty.png" ;
-                marker_lake_tseuzier.icon = img_lac ;
-                break ;
-            case 3 :
-                img_lac.url = "images/lake_nearlyfull.png" ;
-                marker_lake_tseuzier.icon = img_lac ;
-                break ;
-            case 4 :
-                img_lac.url = "images/lake_full.png" ;
-                marker_lake_tseuzier.icon = img_lac ;
-                break ;
-            case 5 :
-                img_lac.url = "images/lake_nearlyfull.png" ;
-                marker_lake_tseuzier.icon = img_lac ;
-                break ;
-            case 6 :
-                img_lac.url = "images/lake_nearlyempty.png" ;
-                marker_lake_tseuzier.icon = img_lac ;
-                break ;
-            case 7 :
-                img_lac.url = "images/lake_empty.png" ;
-                marker_lake_tseuzier.icon = img_lac ;
-                break ;
-            case 8 :
-                img_lac.url = "images/lake_nearlyempty.png" ;
-                marker_lake_tseuzier.icon = img_lac ;
-                break ;
-            case 9 :
-                img_lac.url = "images/lake_nearlyfull.png" ;
-                marker_lake_tseuzier.icon = img_lac ;
-                break ;
-            case 10 :
-                img_lac.url = "images/lake_full.png" ;
-                marker_lake_tseuzier.icon = img_lac ;
-                break ;
-            case 11 :
-                img_lac.url = "images/lake_nearlyfull.png" ;
-                marker_lake_tseuzier.icon = img_lac ;
-                break ;
-            case 12 :
-                img_lac.url = "images/lake_nearlyempty.png" ;
-                marker_lake_tseuzier.icon = img_lac ;
-                break ;
+
+            switch (lvl) {
+                case 0 :
+                    marker_lake_tseuzier.setIcon(img_lake_full) ;
+                    break;
+                case 1 :
+                    marker_lake_tseuzier.setIcon(img_lake_nearly_full);
+                    break;
+                case 2 :
+                    marker_lake_tseuzier.setIcon(img_lake_nearly_empty);
+                    break;
+                case 3 :
+                    marker_lake_tseuzier.setIcon(img_lake_empty);
+                    break;
+            }
+
+
         }
 
 
 
-    }
+
 
 
